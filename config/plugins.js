18c59fb17a8a2fb732e179c04d57bebe87795acb
oussamaassal <ahmedoussama.assal@esprit.tn>
2025-01-04 19:35:46 +0100

    google provider settings added

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  "users-permissions": {
+    config: {
+      providers: {
+        linkedin: {
+          key: env("LINKEDIN_CLIENT_ID"),
+          secret: env("LINKEDIN_CLIENT_SECRET"),
+          callback: `${env("APP_URL")}/api/connect/google/callback`,
+          scope: ["openid", "profile", "email"],
+        },
+      },
+    },
+  },
+});
